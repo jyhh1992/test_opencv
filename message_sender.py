a0c578d232bc9ebaa3e07323ad564ed859c8719c
jyhh1992 <jyhh1992@gmail.com>
2021-12-02 17:36:12 +0900

    tongsin

--- message_sender.py
+++ message_sender.py
@@ -6,12 +6,7 @@
 
 import socket
 
+
 sender = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# socket 통신준비
-
-sender.bind('192.168.16.24', 7778)
-# 바인딩
-
-sender.recvfrom(1024)
-# 통신 사이즈 설정
+sender.sendto(str.encode('Hello Sender'),'192.168.16.24', 7778)
 
