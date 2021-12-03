e4d8670cc55b2750955ca661838940069c2e8667
jyhh1992 <jyhh1992@gmail.com>
2021-12-03 10:30:15 +0900

    sada

--- meg_receiver.py
+++ meg_receiver.py
@@ -1,10 +1,28 @@
-def func():
-   pass
-   return
+import socket
+
+def func_recev(ip,port):
+    recevier = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # socket 통신준비
+
+    recevier.bind((ip, port))
+    # 바인딩
+
+    while True:
+        bytepair = recevier.recvfrom(1024)
+        # 통신 사이즈 설정
+
+        message = bytepair[0]
+        address = bytepair[1]
+
+        print(message, '.',address)
+
+    return
 
 if __name__ == '__main__':
-      try:
-         pass
-      except Exception as e:
-         pass
+
+    func_recev('192.168.16.24', 7778)
+    try:
+        pass
+    except Exception as e:
+        pass
 
--- meg_sender.py
+++ meg_sender.py
@@ -1,10 +1,15 @@
-def func():
-   pass
-   return
+import socket
+
+def func_send(ip,port):
+    sender = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    sender.sendto(str.encode('Hello Sender'),ip,port)
+    return
 
 if __name__ == '__main__':
-      try:
-         pass
-      except Exception as e:
-         pass
+
+    func_send('192.168.16.24', 7778)
+    try:
+        pass
+    except Exception as e:
+        pass
 
