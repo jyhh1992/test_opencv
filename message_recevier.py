b69038691e88877ac253e18a43c36fe9aee86382
jyhh1992 <jyhh1992@gmail.com>
2021-12-03 09:04:55 +0900

    call community

---
+++ message_recevier.py
@@ -0,0 +1,22 @@
+# channel open
+# ip port community rule
+# socket call
+# send something
+# 
+
+import socket
+
+recevier = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# socket 통신준비
+
+recevier.bind(('192.168.16.24', 7778))
+# 바인딩
+
+while True:
+    bytepair = recevier.recvfrom(1024)
+    # 통신 사이즈 설정
+
+    message = bytepair[0]
+    address = bytepair[1]
+
+    print(message, '.',address)
---
+++ message_sender.py
@@ -0,0 +1,11 @@
+# channel open
+# ip port community rule
+# socket call
+# send something
+# 
+
+import socket
+
+
+sender = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+sender.sendto(str.encode('Hello Sender'),'192.168.16.24', 7778)
